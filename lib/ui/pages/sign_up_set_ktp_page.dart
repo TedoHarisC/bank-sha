9d167b04c2b832d27ff46e002b15eef45000dca3
TedoHarisC <tedoharischandra29@gmail.com>
2023-05-19 10:41:38 +0700

    Membuat User Mode

--- lib/ui/pages/sign_up_set_ktp_page.dart
+++ lib/ui/pages/sign_up_set_ktp_page.dart
@@ -1,15 +1,33 @@
+import 'dart:io';
+
 import 'package:bank_sha/models/sign_up_form_model.dart';
+import 'package:bank_sha/shared/shared_methods.dart';
 import 'package:bank_sha/shared/theme.dart';
 import 'package:bank_sha/ui/widgets/buttons.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
-class SignUpSetKTPPage extends StatelessWidget {
+class SignUpSetKTPPage extends StatefulWidget {
   final SignUpFormModel data;
 
   const SignUpSetKTPPage({
     super.key,
     required this.data,
   });
+
+  @override
+  State<SignUpSetKTPPage> createState() => _SignUpSetKTPPageState();
+}
+
+class _SignUpSetKTPPageState extends State<SignUpSetKTPPage> {
+  XFile? selectedImage;
+
+  bool validate() {
+    if (selectedImage == null) {
+      return false;
+    }
+    return true;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -49,31 +67,40 @@ class SignUpSetKTPPage extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Container(
-                  width: 120,
-                  height: 120,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: lightBackgroundColor,
-                  ),
-                  child: Center(
-                    child: Image.asset(
-                      'assets/ic_upload.png',
-                      width: 32,
+                GestureDetector(
+                  onTap: () async {
+                    final image = await selectImage();
+                    setState(() {
+                      selectedImage = image;
+                    });
+                  },
+                  child: Container(
+                    width: 120,
+                    height: 120,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: lightBackgroundColor,
+                      image: selectedImage == null
+                          ? null
+                          : DecorationImage(
+                              fit: BoxFit.cover,
+                              image: FileImage(
+                                File(
+                                  selectedImage!.path,
+                                ),
+                              ),
+                            ),
                     ),
+                    child: selectedImage != null
+                        ? null
+                        : Center(
+                            child: Image.asset(
+                              'assets/ic_upload.png',
+                              width: 32,
+                            ),
+                          ),
                   ),
                 ),
-                // Container(
-                //   width: 120,
-                //   height: 120,
-                //   decoration: BoxDecoration(
-                //     shape: BoxShape.circle,
-                //     color: lightBackgroundColor,
-                //     image: const DecorationImage(
-                //       image: AssetImage('assets/img_profile.png'),
-                //     ),
-                //   ),
-                // ),
                 const SizedBox(height: 16),
                 Text(
                   'Passport/ID Card',
@@ -87,7 +114,11 @@ class SignUpSetKTPPage extends StatelessWidget {
                 CustomFilledButton(
                   title: 'Continue',
                   onPressed: () {
-                    Navigator.pushNamed(context, '/sign-up-success');
+                    if (validate()) {
+                      Navigator.pushNamed(context, '/sign-up-success');
+                    } else {
+                      showCustomSnackbar(context, 'Gambar tidak boleh kosong');
+                    }
                   },
                 )
               ],
