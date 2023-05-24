53cae9648a5c41d0aaadb06aaa98daccd8ed65ec
TedoHarisC <tedoharischandra29@gmail.com>
2023-05-24 09:10:12 +0700

    Membuat Model & User Service

--- lib/ui/pages/profile_edit_page.dart
+++ lib/ui/pages/profile_edit_page.dart
@@ -1,10 +1,34 @@
+import 'package:bank_sha/blocs/auth/auth_bloc.dart';
 import 'package:bank_sha/shared/theme.dart';
 import 'package:bank_sha/ui/widgets/buttons.dart';
 import 'package:bank_sha/ui/widgets/forms.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class ProfileEditPage extends StatelessWidget {
+class ProfileEditPage extends StatefulWidget {
   const ProfileEditPage({super.key});
+
+  @override
+  State<ProfileEditPage> createState() => _ProfileEditPageState();
+}
+
+class _ProfileEditPageState extends State<ProfileEditPage> {
+  final usernameController = TextEditingController(text: '');
+  final nameController = TextEditingController(text: '');
+  final emailController = TextEditingController(text: '');
+  final passwordController = TextEditingController(text: '');
+
+  @override
+  void initState() {
+    super.initState();
+    final authState = context.read<AuthBloc>().state;
+    if (authState is AuthSuccess) {
+      usernameController.text = authState.user.username!;
+      nameController.text = authState.user.name!;
+      emailController.text = authState.user.email!;
+      passwordController.text = authState.user.password!;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,24 +52,28 @@ class ProfileEditPage extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 //NOTE : USERNAME INPUT
-                const CustomFormField(
+                CustomFormField(
                   title: "Username",
+                  controller: usernameController,
                 ),
                 const SizedBox(height: 16),
                 //NOTE : FULL NAME INPUT
-                const CustomFormField(
+                CustomFormField(
                   title: "Full Name",
+                  controller: nameController,
                 ),
                 const SizedBox(height: 16),
                 //NOTE : EMAIL INPUT
-                const CustomFormField(
+                CustomFormField(
                   title: "Email Address",
+                  controller: emailController,
                 ),
                 const SizedBox(height: 16),
                 //NOTE : PASSWORD INPUT
-                const CustomFormField(
+                CustomFormField(
                   title: "Password",
                   obscureText: true,
+                  controller: passwordController,
                 ),
                 const SizedBox(height: 30),
                 //NOTE : BUTTON SIGN IN
