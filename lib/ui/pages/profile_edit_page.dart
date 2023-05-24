865777be1c66be50770d05fe68247eafe3af7cc4
TedoHarisC <tedoharischandra29@gmail.com>
2023-05-24 09:25:30 +0700

    Menambahkan Halaman edit profile

--- lib/ui/pages/profile_edit_page.dart
+++ lib/ui/pages/profile_edit_page.dart
@@ -1,4 +1,6 @@
 import 'package:bank_sha/blocs/auth/auth_bloc.dart';
+import 'package:bank_sha/models/user_edit_form_model.dart';
+import 'package:bank_sha/shared/shared_methods.dart';
 import 'package:bank_sha/shared/theme.dart';
 import 'package:bank_sha/ui/widgets/buttons.dart';
 import 'package:bank_sha/ui/widgets/forms.dart';
@@ -36,58 +38,86 @@ class _ProfileEditPageState extends State<ProfileEditPage> {
       appBar: AppBar(
         title: const Text('Edit Profile'),
       ),
-      body: ListView(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 24,
-        ),
-        children: [
-          const SizedBox(height: 30),
-          Container(
-            padding: const EdgeInsets.all(22),
-            decoration: BoxDecoration(
-              color: whiteColor,
-              borderRadius: BorderRadius.circular(20),
+      body: BlocConsumer<AuthBloc, AuthState>(
+        listener: (context, state) {
+          if (state is AuthFailed) {
+            showCustomSnackbar(context, state.e);
+          }
+
+          if (state is AuthSuccess) {
+            Navigator.pushNamedAndRemoveUntil(
+                context, "/profile-edit-success", (route) => false);
+          }
+        },
+        builder: (context, state) {
+          if (state is AuthLoading) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          return ListView(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 24,
             ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                //NOTE : USERNAME INPUT
-                CustomFormField(
-                  title: "Username",
-                  controller: usernameController,
+            children: [
+              const SizedBox(height: 30),
+              Container(
+                padding: const EdgeInsets.all(22),
+                decoration: BoxDecoration(
+                  color: whiteColor,
+                  borderRadius: BorderRadius.circular(20),
                 ),
-                const SizedBox(height: 16),
-                //NOTE : FULL NAME INPUT
-                CustomFormField(
-                  title: "Full Name",
-                  controller: nameController,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    //NOTE : USERNAME INPUT
+                    CustomFormField(
+                      title: "Username",
+                      controller: usernameController,
+                    ),
+                    const SizedBox(height: 16),
+                    //NOTE : FULL NAME INPUT
+                    CustomFormField(
+                      title: "Full Name",
+                      controller: nameController,
+                    ),
+                    const SizedBox(height: 16),
+                    //NOTE : EMAIL INPUT
+                    CustomFormField(
+                      title: "Email Address",
+                      controller: emailController,
+                    ),
+                    const SizedBox(height: 16),
+                    //NOTE : PASSWORD INPUT
+                    CustomFormField(
+                      title: "Password",
+                      obscureText: true,
+                      controller: passwordController,
+                    ),
+                    const SizedBox(height: 30),
+                    //NOTE : BUTTON SIGN IN
+                    CustomFilledButton(
+                      title: 'Update Now',
+                      onPressed: () {
+                        context.read<AuthBloc>().add(
+                              AuthUpdateUser(
+                                UserEditFormModel(
+                                  username: usernameController.text,
+                                  name: nameController.text,
+                                  email: emailController.text,
+                                  password: passwordController.text,
+                                ),
+                              ),
+                            );
+                      },
+                    )
+                  ],
                 ),
-                const SizedBox(height: 16),
-                //NOTE : EMAIL INPUT
-                CustomFormField(
-                  title: "Email Address",
-                  controller: emailController,
-                ),
-                const SizedBox(height: 16),
-                //NOTE : PASSWORD INPUT
-                CustomFormField(
-                  title: "Password",
-                  obscureText: true,
-                  controller: passwordController,
-                ),
-                const SizedBox(height: 30),
-                //NOTE : BUTTON SIGN IN
-                CustomFilledButton(
-                  title: 'Update Now',
-                  onPressed: () {
-                    Navigator.pushNamedAndRemoveUntil(
-                        context, "/profile-edit-success", (route) => false);
-                  },
-                )
-              ],
-            ),
-          ),
-        ],
+              ),
+            ],
+          );
+        },
       ),
     );
   }
